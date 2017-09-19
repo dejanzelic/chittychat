cabd04204fdc97eb2d8beeb4ae8809bf8fe8e6b9
Dejan Zelic <dejanzelic@gmail.com>
2017-09-19 15:49:36 -0700

    minor changes

--- aws_deploy/s3_upload.sh
+++ aws_deploy/s3_upload.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-aws s3 --recursive cp ./ s3://appsecusa-7d2b277154db/source/chitty_chat --exclude "node_modules/*" --exclude "package-lock.json"
+aws s3 --recursive cp ../ s3://appsecusa-7d2b277154db/source/chitty_chat --exclude "node_modules/*" --exclude "package-lock.json" --exclude ".git/*" --exclude ".vagrant/*"
